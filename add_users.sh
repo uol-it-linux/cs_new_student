afa9dd6102d12b292a5de09a74e268e98ea7fc7a
Jon Whitwell <78543293+issjwh@users.noreply.github.com>
2023-09-21 09:40:36 +0100

    tweaks / debug

--- add_users.sh
+++ add_users.sh
@@ -3,17 +3,17 @@
 path="/mnt/eufs003/student_lnxhome01"
 symlink="/uolstore/home/student_lnxhome01"
 log_file="/var/log/script.log"
-userlist="~/cs_new_student/user_list"
-testlist="~/cs_new_student/test_list"
+userlist="/root/cs_new_student/user_list"
+testlist="~/root/cs_new_student/test_list"
 
 # Initialize the log file
 echo "Script started at $(date)" > "$log_file"
 
 # Check if the user_list file exists
-if [ ! -f "$userlist" ]; then
-    echo "Error: The user_list file '$userlist' does not exist." | tee -a "$log_file"
-    exit 1
-fi
+#if [ ! -f "$userlist" ]; then
+#    echo "Error: The user_list file '$userlist' does not exist." | tee -a "$log_file"
+#    exit 1
+#fi
 
 readarray -t user_list < "$testlist"
 
@@ -38,7 +38,7 @@ for i in "${!user_list[@]}"; do
    chmod 700 "$source_dir"
    
    # Create the symbolic link
-   ln -s "/home/csunix/${user_list[$i]}" "/$symlink/${user_list[$i]}"
+   ln -s "/home/csunix/${user_list[$i]}" "$symlink/${user_list[$i]}"
    if [ $? -ne 0 ]; then
        echo "Error: Failed to create symbolic link for '${user_list[$i]}'." | tee -a "$log_file"
        # You can choose to exit or continue with the loop based on your preference
